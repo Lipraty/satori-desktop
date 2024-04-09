fa7a47d084cbdde11c8ccb31ee42624750487734
Lipraty <sis1943@outlook.com>
2024-04-09 12:21:16 +0800

    chore: use `@scope` css style

--- src/views/TitleBar.tsx
+++ src/views/TitleBar.tsx
@@ -7,7 +7,20 @@ export interface TitleBarProps {
 export const TitleBar: React.FC<TitleBarProps> = (props) => {
   const { title = 'Satori App for Desktop' } = props;
   return (
-    <div className="title-bar">
+    <div className=''>
+      <style>
+        {`
+          @scope {
+            :scope {
+              -webkit-app-region: drag;
+              height: 30px;
+              padding: 0 12px;
+              display: flex;
+              align-items: center;
+            }
+          }
+        `}
+      </style>
       <span>{title}</span>
     </div>
   );
