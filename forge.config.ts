b73c6a28688639366a81812b0089498b5e576f99
Lipraty <sis1943@outlook.com>
2024-04-21 17:36:23 +0800

    fix: fix resolve entry module `preload`'s path

--- forge.config.ts
+++ forge.config.ts
@@ -24,7 +24,7 @@ const config: ForgeConfig = {
           config: 'vite.main.config.ts',
         },
         {
-          entry: 'src/common/preload.ts',
+          entry: 'src/main/preload.ts',
           config: 'vite.preload.config.ts',
         },
       ],
