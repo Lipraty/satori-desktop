662d2840dcfe5667b9e47c04160700e85665895f
MicroBlock <microcber@gmail.com>
2024-05-17 17:41:38 +0800

    refactor: useThemeListener to useSyncExternalStore

--- src/renderer/hooks/useThemeListener.ts
+++ src/renderer/hooks/useThemeListener.ts
@@ -1,36 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useSyncExternalStore } from 'react';
 
-const MATCH_MEDIA_QUERY_DARK_THEME = '(prefers-color-scheme: dark)'
-const MATCH_MEDIA_QUERY_LIGHT_THEME = '(prefers-color-scheme: light)'
+const MATCH_MEDIA_QUERY_LIGHT_THEME = '(prefers-color-scheme: light)';
+
+let isDarkTheme = false;
+
+const getIsDarkTheme = () => isDarkTheme;
+
+const subscribe = (callback: () => void) => {
+  const lightThemeMatchMedia = window.matchMedia(MATCH_MEDIA_QUERY_LIGHT_THEME);
+
+  const updateThemeCallback = ()=>{
+    isDarkTheme = !lightThemeMatchMedia.matches;
+    callback();
+  }
+  lightThemeMatchMedia.addEventListener('change', updateThemeCallback);
+  
+  updateThemeCallback()
+  return () => {
+    lightThemeMatchMedia.removeEventListener('change', updateThemeCallback);
+  };
+};
 
 export const useThemeListener = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
-
-  useEffect(() => {
-    if (!('matchMedia' in window && 'addEventListener' in window.matchMedia(MATCH_MEDIA_QUERY_DARK_THEME))) {
-      return
-    }
-
-    const lightThemeMatchMedia = window.matchMedia(MATCH_MEDIA_QUERY_LIGHT_THEME)
-    const darkThemeMatchMedia = window.matchMedia(MATCH_MEDIA_QUERY_DARK_THEME)
-
-    const updateThemeCallbackWrapper = (theme: 'light' | 'dark') => (ev: MediaQueryListEvent) => {
-      if (ev.matches) {
-        setIsDarkTheme(theme === 'dark')
-      }
-    }
-
-    const updateThemeCallbackDark = updateThemeCallbackWrapper('dark')
-    const updateThemeCallbackLight = updateThemeCallbackWrapper('light')
-
-    lightThemeMatchMedia.addEventListener('change', updateThemeCallbackLight)
-    darkThemeMatchMedia.addEventListener('change', updateThemeCallbackDark)
-
-    return () => {
-      lightThemeMatchMedia.removeEventListener('change', updateThemeCallbackLight)
-      darkThemeMatchMedia.removeEventListener('change', updateThemeCallbackDark)
-    }
-  }, [isDarkTheme, setIsDarkTheme])
-
-  return isDarkTheme
-}
+  return useSyncExternalStore(subscribe, getIsDarkTheme);
+};
