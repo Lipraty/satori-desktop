742fc4a0a4e4c3311c41248b2b87e97698b9595f
Lipraty <sis1943@outlook.com>
2024-05-25 13:08:41 +0800

    chore: style adjustment

--- src/renderer/views/Messaging.tsx
+++ src/renderer/views/Messaging.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react'
-import { Avatar, Body1, Button, Caption1, Card, CardFooter, CardHeader, CardPreview, Input } from '@fluentui/react-components'
+import { Avatar, Body1, Button, Caption1, Card, CardFooter, CardPreview, Input, TabList, Tab } from '@fluentui/react-components'
 import { VirtualizerScrollViewDynamic } from '@fluentui/react-components/unstable'
 import { } from '@shikitor/core'
 // import {} from '@shikitor/react'
@@ -18,6 +18,10 @@ export const MessagingView = () => {
     , [])
 
   const content = '你说的对，但是《koishi》是由关门歇业自主研发的一款跨平台，高性能的机器人框架。p框架发生在一个被称作「node.js」的幻想世界，在这里，被内存选中的元素将被授予「类型」导引typescript之力。你将扮演一位名为「开发者」的神秘角色在自由的旅行中邂逅报错各异、能力独特的彩色括号们，和他们一起击败any，找回失散的类型——同时，逐步发掘「世革马」的真相。'
+  const messageActions = [{
+
+  }]
+
 
   return (
     <>
@@ -29,8 +33,21 @@ export const MessagingView = () => {
           margin: '0 -12px',
         }}>
           <List.Subheader title="PINNED" />
-          <ListItem key={'key2'} title="Shigma" avatar />
           <ListItem key={'key1'} title="Shigma" avatar subtitle={content} />
+          <List.Subheader title="MESSAGES" />
+          <ListItem key={'key2'} title="Maiko Tan" avatar subtitle={content} />
+          <ListItem key={'key3'} title="Il Harper" avatar subtitle={content} />
+          <ListItem key={'key4'} title="Koishi" avatar subtitle={content} />
+          <List.Item>
+            <Caption1 style={{
+              display: 'block',
+              width: '100%',
+              textAlign: 'center',
+            }}>
+              <Icon name='Add' />
+              New Message
+            </Caption1>
+          </List.Item>
         </List>
       </ViewBox>
       <ViewBox transparent style={{
@@ -38,62 +55,68 @@ export const MessagingView = () => {
         padding: '0',
       }}>
         <div className='messaging-titlebar'>
-          <h2 className='messaging-titlebar__title'>Shigma</h2>
-          <hr />
+          <Avatar name="Shigma" size={28} />
+          <span className='messaging-titlebar__title'>Shigma</span>
+          <TabList size='large'>
+            <Tab value='chat'>Chat</Tab>
+            <Tab value='files'>Files</Tab>
+            <Tab value='photos'>Photos</Tab>
+          </TabList>
+          <div style={{
+            flex: '1',
+            maxWidth: '100%',
+          }}/>
+          <Button shape='circular' appearance='transparent' icon={<Icon name='PersonInfo' bundle/>} />
         </div>
         <div className='messaging-context'>
-          <VirtualizerScrollViewDynamic numItems={2} itemSize={20}>
+          <span className='message-timestamp'>Today</span>
+          <VirtualizerScrollViewDynamic numItems={1} itemSize={20}>
             {(index: number) => {
               return (
                 <div className='message'>
-                  <Avatar name="Shigma" size={40} />
-                  <div className='message__content'>
-                    <Body1 style={{
-                      margin: '0 12px',
-                    }}>
+                  <Avatar className='message-avatar' name="Shigma" size={40} />
+                  <div className='message-content'>
+                    <Body1 className='message-content__username'>
                       Shigma
                     </Body1>
-                    <Card appearance='filled' style={{
-                      margin: '0 12px 1rem 12px',
-                      boxShadow: 'var(--shadow2)',
-                    }}>
+                    <Card appearance='filled'>
                       <CardPreview style={{
                         padding: '12px',
                       }}>
-                        {content}
+                        {'content'}
                       </CardPreview>
-
-                      <CardFooter>
-                        <Button>Btn1</Button>
-                        <Button>Btn2</Button>
-                      </CardFooter>
                     </Card>
-                    
-                    <Card appearance='filled' style={{
-                      margin: '0 12px 1rem 12px',
-                      boxShadow: 'var(--shadow2)',
-                    }}>
+                    <Card appearance='filled'>
                       <CardPreview style={{
                         padding: '12px',
                       }}>
                         {content}
                       </CardPreview>
-
                       <CardFooter>
                         <Button>Btn1</Button>
                         <Button>Btn2</Button>
                       </CardFooter>
                     </Card>
                   </div>
-
                 </div>
               )
             }}
           </VirtualizerScrollViewDynamic>
         </div>
-
         <div className='messaging-sender'>
-          await <code>@shikitor/react</code>
+          <div className='messaging-sender__actions'>
+            <Button appearance='transparent' icon={<Icon name='TextEditStyle' bundle/>} />
+            <Button appearance='transparent' icon={<Icon name='Image' bundle/>} />
+            <Button appearance='transparent' icon={<Icon name='Attach' bundle/>} />
+            <Button appearance='transparent' icon={<Icon name='Emoji' bundle/>} />
+            <div style={{
+              flex: '1',
+              maxWidth: '100%'
+            }}/>
+          </div>
+          <Input size='large' placeholder="Type a message" appearance='filled-darker' contentAfter={
+            <Button appearance='transparent' icon={<Icon name='Send' bundle/>} />
+          }/>
         </div>
       </ViewBox>
     </>
