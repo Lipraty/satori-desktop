0fc3457fe6dcb6a28c61873f4726755c9c69bdc9
Lipraty <sis1943@outlook.com>
2024-08-26 09:51:13 +0800

    chore: use `KoishiTheme` colors in default theme

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react"
-import { Avatar, FluentProvider, webDarkTheme, webLightTheme } from "@fluentui/react-components"
+import { Avatar, BrandVariants, createDarkTheme, createLightTheme, FluentProvider, Theme } from "@fluentui/react-components"
 
 import { useThemeListener } from "@renderer/hooks/use-theme-listener"
 import { useCurrentView } from "@renderer/hooks/view-manager"
@@ -13,6 +13,36 @@ export const App = () => {
   const darkThemeStatus = useThemeListener()
   // const [loading, setLoading] = useState<boolean>(true)
   const { setCurrentView, currentView } = useCurrentView()
+
+  const koishiTheme: BrandVariants = {
+    10: "#030205",
+    20: "#181424",
+    30: "#261F40",
+    40: "#322958",
+    50: "#3E3372",
+    60: "#4A3D8C",
+    70: "#5748A4",
+    80: "#6756AC",
+    90: "#7565B4",
+    100: "#8474BC",
+    110: "#9283C4",
+    120: "#A092CC",
+    130: "#AFA2D4",
+    140: "#BDB2DB",
+    150: "#CBC2E3",
+    160: "#D9D2EB"
+  };
+
+  const lightTheme: Theme = {
+    ...createLightTheme(koishiTheme),
+  };
+
+  const darkTheme: Theme = {
+    ...createDarkTheme(koishiTheme),
+  };
+
+  // darkTheme.colorBrandForeground1 = koishiTheme[110];
+  // darkTheme.colorBrandForeground2 = koishiTheme[120];
 
   const SidebarItem = useCallback(
     ({ icon, label, name }: { icon: IconNames, label: string, name: string }) =>
@@ -28,7 +58,7 @@ export const App = () => {
   return (
     <>
       <TitleBar title='Satori App for Desktop' icon="assets/icons/icon.png" />
-      <FluentProvider theme={darkThemeStatus ? webDarkTheme : webLightTheme} style={{
+      <FluentProvider theme={darkThemeStatus ? darkTheme : lightTheme} style={{
         display: 'flex',
         height: 'calc(100vh - 44px)',
         width: '100%',
@@ -47,7 +77,7 @@ export const App = () => {
               status: 'available',
             }} image={{
               src: 'https://koishi.chat/logo.png'
-            }}/>
+            }} />
           </Sidebar.Item>
         </Sidebar>
         <main>
