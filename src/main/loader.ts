30b928e9c0503ce0c3ddb1d73bb0cb4f8a160585
Lipraty <sis1943@outlook.com>
2024-10-04 23:32:14 +0800

    chore: add some little note

--- src/main/loader.ts
+++ src/main/loader.ts
@@ -1,1 +1,12 @@
-
+/**
+ * TODO
+ * 
+ * 1. Load the app
+ * 2. Start the app database
+ * 3. Read the settings
+ * 4. Start the app external server
+ * 5. Create the app window
+ * 6. Load the plugins
+ * 
+ * HMR?
+ */
--- src/main/network.ts
+++ src/main/network.ts
@@ -1,6 +1,27 @@
 import { SatoriAdapter as AdapterSatori } from "@satorijs/adapter-satori"
 import { DiscordBot as AdapterDiscord } from '@satorijs/adapter-discord'
 
+import { Context, Service } from "./context"
+
+// NOTE
+// To eliminate ambiguity in the code
+// in the main process we will use
+// the term "adapter" to refer to the
+// network adapter service.
+
+declare module './context' {
+  interface Context {
+    adapter: SatoriAdapterService
+  }
+
+  // NOTE
+  // in renderer process, we will use
+  // the term "network".
+  interface Events {
+    'network/ready': void
+  }
+}
+
 export const appNetworks = {
   satori: {
     name: 'Satori',
@@ -17,3 +38,16 @@ export const appNetworks = {
 export type AppNetwork = keyof typeof appNetworks
 
 export const appNetworksList = Object.keys(appNetworks) as AppNetwork[]
+
+/**
+ * The network adapter service.
+ * 
+ * This service is responsible for managing the network adapters.
+ */
+export default class SatoriAdapterService extends Service {
+  static inject = ['ipc']
+
+  constructor(ctx: Context) {
+    super(ctx, 'adapter')
+  }
+}
