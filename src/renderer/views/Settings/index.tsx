9bcdfd5964147f5c9b1bf74535ff7350212c182c
Lipraty <i@lonay.me>
2024-12-08 22:54:55 +0800

    chore: simplify SettingsView title

--- src/renderer/views/Settings/index.tsx
+++ src/renderer/views/Settings/index.tsx
@@ -5,12 +5,7 @@ import { ViewBox } from '@renderer/components/ViewBox'
 import { SettingsAbout } from './About'
 
 export const SettingsView = () => <>
-  <ViewBox style={{
-    flexDirection: 'column',
-  }} transparent>
-    <Title3 as='span' style={{
-      marginBottom: '10px',
-    }}>Settings</Title3>
+  <ViewBox title='Settings'>
     <SettingsAbout />
   </ViewBox>
 </>
