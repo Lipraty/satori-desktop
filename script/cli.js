4f46a08d93bbb529d8d7b882ae5c6d22b28f1e1b
Lipraty <i@lonay.me>
2024-12-14 18:25:22 +0800

    feat: add a element's junk terminal output filter
    refer: https://github.com/sindresorhus/run-electron/blob/main/cli.js

---
+++ script/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import { spawn } from 'node:child_process'
+import process from 'node:process'
+
+process.env.FORCE_COLOR = 1
+
+const isJunkOutput = (output) => {
+  return [
+    /\d+-\d+-\d+ \d+:\d+:\d+\.\d+ Electron(?: Helper)?\[\d+:\d+] /,
+    /\[\d+:\d+\/|\d+\.\d+:ERROR:CONSOLE\(\d+\)]/,
+    /ALSA lib [a-z]+\.c:\d+:\([a-z_]+\)/,
+    /\+\[IMK.*\]/
+  ].some(pattern => pattern.test(output))
+}
+
+const filterJunkOutput = (data) => {
+  const output = data.toString()
+  if (!isJunkOutput(output)) {
+    process.stdout.write(output)
+  }
+}
+
+const [,, ...args] = process.argv
+
+if (args.length === 0) {
+  console.error('Usage: electron-filter [command] [args...]')
+  process.exit(1)
+}
+
+const subprocess = spawn(args[0], args.slice(1), { stdio: ['inherit', 'pipe', 'pipe'] })
+
+subprocess.stdout.on('data', filterJunkOutput)
+subprocess.stderr.on('data', filterJunkOutput)
+
+subprocess.on('close', (code) => {
+  process.exit(code)
+})
