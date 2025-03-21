5dbac624623fbd2582095ce2b1112c4813a8109d
Lipraty <i@lonay.me>
2025-03-22 01:16:23 +0800

    chore: type check confrontation

--- src/internal/database.ts
+++ src/internal/database.ts
@@ -1,19 +1,25 @@
 import { Context } from 'cordis'
-import * as minato from 'minato'
+import * as Minato from 'minato'
+//@see https://stackoverflow.com/questions/59906323/typescript-skiplibcheck-still-checking-node-modules-libs
+const minato = require('minato')
 
 declare module 'cordis' {
   interface Context {
     [minato.Types]: Types
     [minato.Tables]: Tables
+    [Context.Database]: Context.Database<this>
+  }
+  namespace Context {
+    interface Database<C extends Context = Context> { }
   }
 }
 
-export interface Tables extends minato.Tables { }
+export interface Tables extends Minato.Tables { }
 
-export interface Types extends minato.Types { }
+export interface Types extends Minato.Types { }
 
-export class Database extends minato.Database {
-  constructor(ctx: Context) {
+export class Database<S extends Tables = Tables, T extends Types = Types, C extends Context = Context> extends Minato.Database<S, T, C> {
+  constructor(ctx: C) {
     super(ctx)
   }
 }
