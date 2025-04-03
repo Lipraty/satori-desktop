d4df1d1b40290492b5fe31fa5f7d8ea6adb35b7c
Lipraty <i@lonay.me>
2025-04-03 17:30:56 +0800

    feat: add `useContext`.
    see: https://github.com/cordiverse/webui/blob/main/packages/client/client/context.ts

--- src/client/context.ts
+++ src/client/context.ts
@@ -1,6 +1,16 @@
 import * as cordis from 'cordis'
-import { App, createApp } from 'vue'
+import { App, createApp, inject, InjectionKey, markRaw, onScopeDispose } from 'vue'
 import Root from './App.vue'
+
+const SatoriCoontext = Symbol('context') as InjectionKey<Context>
+
+export function useContext() {
+  const parent = inject(SatoriCoontext)!
+  const fork = parent.plugin(()=>{})
+  console.log('fork', fork)
+  onScopeDispose(fork.dispose)
+  return fork.ctx
+}
 
 export class Context extends cordis.Context {
   app: App
@@ -8,9 +18,11 @@ export class Context extends cordis.Context {
   constructor() {
     super()
     this.app = createApp(Root)
-
+    this.app.provide(SatoriCoontext, this)
     this.on('ready', () => {
       this.app.mount('#app')
     })
   }
 }
+
+markRaw(cordis.Context.prototype)
