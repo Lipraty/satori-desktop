4726d265461e1da1d8cf3ecf2b449dc99fff0731
Lipraty <i@lonay.me>
2025-04-28 13:54:37 +0800

    feat(renderer): add sidebar animation

--- packages/renderer/src/components/root.vue
+++ packages/renderer/src/components/root.vue
@@ -2,28 +2,35 @@
   <satori-system-bar />
   <div class="satori-container">
     <nav class="satori-nav">
-      <router-link class="satori-nav-item" v-for="page in topPages" :to="page.path" ::key="`link-${page.name}`">
-        <satori-icons v-if="page.icon" :name="page.icon" />
+      <router-link class="satori-nav-item" v-for="page in topPages" :to="page.path" ::key="`link-${page.name}`" ondragstart="return false">
+        <satori-icons v-if="page.icon" :name="page.icon" :filled="page.path === current"/>
         <span>{{ page.name }}</span>
       </router-link>
       <satori-spacer />
-      <router-link class="satori-nav-item" v-for="page in bottomPages" :to="page.path" ::key="`link-${page.name}`">
-        <satori-icons v-if="page.icon" :name="page.icon" />
+      <router-link class="satori-nav-item" v-for="page in bottomPages" :to="page.path" ::key="`link-${page.name}`" ondragstart="return false">
+        <satori-icons v-if="page.icon" :name="page.icon" :filled="page.path === current"/>
         <span>{{ page.name }}</span>
       </router-link>
     </nav>
     <main class="satori-main">
-      <router-view />
+      <router-view v-slot="{ Component, route}">
+        <component :is="Component" />
+      </router-view>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useContext } from '@satoriapp/renderer'
 
 const ctx = useContext()
 const topPages = Object.values(ctx.$router.pages).filter(pager => pager['options'] && pager['options'].position === 'top')
 const bottomPages = Object.values(ctx.$router.pages).filter(pager => pager['options'] && pager['options'].position === 'bottom')
+const current = ref('/')
+ctx.$router.router.afterEach((to, _) => {
+  current.value = to.path
+})
 </script>
 
 <style lang="scss">
@@ -48,10 +55,18 @@ const bottomPages = Object.values(ctx.$router.pages).filter(pager => pager['opti
   flex-direction: column;
   width: 64px;
   margin: 0 10px 10px;
+  gap: 5px;
   .router-link-active {
     background-color: var(--colorNeutralBackground1);
+    &::before {
+      opacity: 1;
+      width: 4px;
+    }
     span {
+      opacity: 0;
       color: var(--colorNeutralForeground2BrandSelected);
+      position: absolute;
+      bottom: 0;
     }
     svg {
       fill: var(--colorNeutralForeground2BrandSelected);
@@ -64,6 +79,7 @@ const bottomPages = Object.values(ctx.$router.pages).filter(pager => pager['opti
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
   gap: 4px;
   width: 64px;
   height: 64px;
@@ -74,10 +90,42 @@ const bottomPages = Object.values(ctx.$router.pages).filter(pager => pager['opti
   letter-spacing: -0.2px;
   line-height: 20.27px;
   text-decoration: none;
+  overflow: hidden;
+  transition: all 0.16s var(--curveEasyEase);
+  * {
+    transition: all 0.16s var(--curveEasyEase);
+  }
   span {
     color: var(--colorNeutralForeground1);
     font-size: var(--fontSizeBase200);
     font-weight: var(--fontWeightRegular);
+    height: auto;
+  }
+  div {
+    transform: translateY(0);
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    height: 1.5rem;
+    width: 0px;
+    border-radius: 0 4px 4px 0;
+    background: var(--colorNeutralForeground2BrandSelected);
+    transform: translateY(-50%) translateX(0);
+    transition: all 0.16s var(--curveEasyEase);
+    opacity: 0;
+  }
+  &:hover {
+    background-color: var(--colorNeutralBackground1);
+    box-shadow: var(--shadow1);
+    span {
+      color: var(--colorNeutralForeground2BrandSelected);
+    }
+    svg {
+      fill: var(--colorNeutralForeground2BrandSelected);
+    }
   }
 }
 .satori-main {
